add4961311be062e9cf9080b2e61620f18a0fbb4
Maxwell Moore <maxwellmoore.work@gmail.com>
2024-08-06 11:13:05 -0600

    Added Log out to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,6 +6,7 @@ import BookmarkIcon from "@mui/icons-material/Bookmark";
 import ChecklistRtlRoundedIcon from "@mui/icons-material/ChecklistRtlRounded";
 import CalendarMonthSharpIcon from "@mui/icons-material/CalendarMonthSharp";
 import WorkRoundedIcon from "@mui/icons-material/WorkRounded";
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
 import { Check } from "@mui/icons-material";
 import { getFilteredApps } from "../services/api/api";
 
@@ -30,59 +31,75 @@ function Sidebar({ setIsLoading, setApplications }) {
   const handleCompanyClick = () => {
     console.log("Company Sidebar Button Clicked");
   };
+  const handleLogoutClick = () => {
+    console.log("Log out clicked");
+  };
 
   return (
     <div className="flex h-full">
       <div
-        className={`flex flex-col justify-start items-center ${expanded ? "w-56" : "w-14"} p-1.5 bg-gray-200 border-r border-gray-400`}
+        className={`flex flex-col justify-between items-center ${expanded ? "w-56" : "w-14"} p-1.5 bg-gray-200 border-r border-gray-400`}
       >
-        <div
-          className={`flex flex-row ${expanded ? "justify-end" : "justify-center"} items-center w-full`}
-        >
-          <ExpandCollapseButton expanded={expanded} onToggle={toggleSidebar} />
-        </div>
-
         <div
           className={`flex flex-col w-full ${expanded ? "items-start" : "items-center"}`}
         >
-          <SideBarFilterButton
-            expanded={expanded}
-            text="All Applications"
-            color="primary"
-            Icon={InboxIcon}
-            onClick={handleAllAppsClick}
-          />
+          <div
+            className={`flex flex-row ${expanded ? "justify-end" : "justify-center"} items-center w-full`}
+          >
+            <ExpandCollapseButton
+              expanded={expanded}
+              onToggle={toggleSidebar}
+            />
+          </div>
+          <div flex flex-col w-full>
+            <SideBarFilterButton
+              expanded={expanded}
+              text="All Applications"
+              color="primary"
+              Icon={InboxIcon}
+              onClick={handleAllAppsClick}
+            />
 
-          <SideBarFilterButton
-            expanded={expanded}
-            text="Bookmarked"
-            color="primary"
-            Icon={BookmarkIcon}
-            onClick={handleBookmarkedClick}
-          />
+            <SideBarFilterButton
+              expanded={expanded}
+              text="Bookmarked"
+              color="primary"
+              Icon={BookmarkIcon}
+              onClick={handleBookmarkedClick}
+            />
 
-          <SideBarFilterButton
-            expanded={expanded}
-            text="Status"
-            color="primary"
-            Icon={ChecklistRtlRoundedIcon}
-            onClick={handleStatusClick}
-          />
+            <SideBarFilterButton
+              expanded={expanded}
+              text="Status"
+              color="primary"
+              Icon={ChecklistRtlRoundedIcon}
+              onClick={handleStatusClick}
+            />
 
-          <SideBarFilterButton
-            expanded={expanded}
-            text="Year"
-            color="primary"
-            Icon={CalendarMonthSharpIcon}
-            onClick={handleYearClick}
-          />
+            <SideBarFilterButton
+              expanded={expanded}
+              text="Year"
+              color="primary"
+              Icon={CalendarMonthSharpIcon}
+              onClick={handleYearClick}
+            />
 
+            <SideBarFilterButton
+              expanded={expanded}
+              text="Company Name"
+              color="primary"
+              Icon={WorkRoundedIcon}
+              onClick={handleCompanyClick}
+            />
+          </div>
+        </div>
+        <div className="flex w-full justify-center items-center">
           <SideBarFilterButton
             expanded={expanded}
-            text="Company Name"
+            text="Log out"
             color="primary"
-            Icon={WorkRoundedIcon}
-            onClick={handleCompanyClick}
+            Icon={LogoutRoundedIcon}
+            onClick={handleLogoutClick}
           />
         </div>
       </div>
